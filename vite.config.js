ec863c6200d5664ede7e6ec071735d086d6f6db2
Usman <tranktp100@gmail.com>
2024-09-09 14:38:34 +0100

    fixed build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Separate the vendor dependencies into its own chunk
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+
 })
